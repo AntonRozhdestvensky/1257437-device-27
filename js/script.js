d849bf753dc148d31707e55cfce522e058ab45ee
AntonRozhdestvensky <57371878+AntonRozhdestvensky@users.noreply.github.com>
2020-03-02 16:28:04 +0300

    Карта сайта + изменен формат картинок + стили для карты

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
+// Форма связи
 const contactFormOpen = document.querySelector('.contactsForm');
 const formPopup = document.querySelector('.contact-form');
 const formClose = document.querySelector('.close-form');
-
+// Карта сайта
+const popupMapOpen = document.querySelector('.openMap');
+const mapOpen = document.querySelector('.popup-map');
+const mapClose = document.querySelector('.close-map');
+// Вычисления формы связи
 contactFormOpen.addEventListener("click", eventForm => {
   eventForm.preventDefault();
   formPopup.classList.add('contact-form-activate');
@@ -10,4 +15,14 @@ contactFormOpen.addEventListener("click", eventForm => {
 formClose.addEventListener("click", eventForm => {
   eventForm.preventDefault();
   formPopup.classList.remove('contact-form-activate');
+});
+// Вычисления карта сайта
+popupMapOpen.addEventListener("click", eventForm => {
+  eventForm.preventDefault();
+  mapOpen.classList.add('popup-map-activate');
+});
+
+mapClose.addEventListener("click", eventForm => {
+  eventForm.preventDefault();
+  mapOpen.classList.remove('popup-map-activate');
 });
